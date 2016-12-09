ba12a422b7b4e187e823248f451407c265981d0e
nipa <nipa@codefx.org>
2016-12-09 11:57:38 +0100

    Use new 'requires transitive' instead of 'requires public'

--- src/org.codefx.demo.advent.calendar/module-info.java
+++ src/org.codefx.demo.advent.calendar/module-info.java
@@ -1,6 +1,6 @@
 module org.codefx.demo.advent.calendar {
 	// list the required modules
-	requires public org.codefx.demo.advent.surprise;
+	requires transitive org.codefx.demo.advent.surprise;
 	// list the packages that will be publicly accessible
 	exports org.codefx.demo.advent.calendar;
 }
