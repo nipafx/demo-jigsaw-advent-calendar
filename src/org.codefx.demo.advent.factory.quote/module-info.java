edecd38a51d19ed503f8d378311404d44d9e5198
nipa <nipa@codefx.org>
2016-12-09 11:44:13 +0100

    Use new 'requires transitive' instead of 'requires public'

--- src/org.codefx.demo.advent.factory.quote/module-info.java
+++ src/org.codefx.demo.advent.factory.quote/module-info.java
@@ -1,6 +1,6 @@
 module org.codefx.demo.advent.factory.quote {
 	// list the required modules
-	requires public org.codefx.demo.advent.surprise;
+	requires transitive org.codefx.demo.advent.surprise;
 	// specify which class provides which service
 	provides org.codefx.demo.advent.surprise.SurpriseFactory
 		with org.codefx.demo.advent.factory.quote.QuoteFactory;
