f884008c83baa8121e1ab04c708da0e21bb00ccd
nipa <nipa@codefx.org>
2017-04-17 15:56:08 +0200

    Use multi-module build

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -6,60 +6,31 @@ mkdir classes
 rm -r mods
 mkdir mods
 
-echo " > compiling and packaging Surprise"
+echo " > compiling modules"
 mkdir classes/org.codefx.demo.advent.surprise
-javac9 \
-	-d classes/org.codefx.demo.advent.surprise \
-	$(find src/org.codefx.demo.advent.surprise -name '*.java')
-jar9 \
-	-c \
+javac9 -d classes \
+	--module-source-path "src" \
+	--add-modules org.codefx.demo.advent.factory.chocolate,org.codefx.demo.advent.factory.quote \
+	--module org.codefx.demo.advent
+
+echo " > packaging modules"
+jar9 --create \
 	--file mods/org.codefx.demo.advent.surprise.jar \
 	-C classes/org.codefx.demo.advent.surprise/ .
-
-echo " > compiling and packaging Calendar"
-mkdir classes/org.codefx.demo.advent.calendar
-javac9 \
-	-p mods \
-	-d classes/org.codefx.demo.advent.calendar \
-	$(find src/org.codefx.demo.advent.calendar -name '*.java')
-jar9 \
-	-c \
+jar9 --create \
 	--file mods/org.codefx.demo.advent.calendar.jar \
 	-C classes/org.codefx.demo.advent.calendar/ .
-
-echo " > compiling and packaging ChocolateFactory"
-mkdir classes/org.codefx.demo.advent.factory.chocolate
-javac9 \
-	-p mods \
-	-d classes/org.codefx.demo.advent.factory.chocolate \
-	$(find src/org.codefx.demo.advent.factory.chocolate -name '*.java')
-jar9 \
-	-c \
+jar9 --create \
 	--file mods/org.codefx.demo.advent.factory.chocolate.jar \
 	-C classes/org.codefx.demo.advent.factory.chocolate/ .
-
-echo " > compiling and packaging QuoteFactory"
-mkdir classes/org.codefx.demo.advent.factory.quote
-javac9 \
-	-p mods \
-	-d classes/org.codefx.demo.advent.factory.quote \
-	$(find src/org.codefx.demo.advent.factory.quote -name '*.java')
-jar9 \
-	-c \
+jar9  --create \
 	--file mods/org.codefx.demo.advent.factory.quote.jar \
 	-C classes/org.codefx.demo.advent.factory.quote/ .
-
-echo " > compiling and packaging Advent"
-mkdir classes/org.codefx.demo.advent
-javac9 \
-	-p mods \
-	-d classes/org.codefx.demo.advent \
-	$(find src/org.codefx.demo.advent -name '*.java')
-jar9 \
-	-c \
+jar9 --create \
 	--file mods/org.codefx.demo.advent.jar \
 	--main-class org.codefx.demo.advent.Main \
 	-C classes/org.codefx.demo.advent/ .
 
 echo " > running Advent"
-java9 -p mods -m org.codefx.demo.advent
+java9 --module-path mods \
+	--module org.codefx.demo.advent
